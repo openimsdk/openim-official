bd85432226294bcb2c73438122d6927cb8a4fb2b
HCX <1874640833@qq.com>
2023-05-17 20:12:29 +0800

    feat(): 底部导航

---
+++ src/components/footerBar/FooterBar.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image"
+
+const FooterBar = () => {
+  return (
+    <div>
+      <footer className="footer px-[8vw] py-10 text-neutral-content ">
+        <div>
+          <span className="footer-title">产品</span>
+          <a className="link-hover link">开源SDK</a>
+          <a className="link-hover link">商用体验demo</a>
+        </div>
+        <div>
+          <span className="footer-title">开发者</span>
+          <a className="link-hover link">文档</a>
+          <a className="link-hover link">GitHub</a>
+        </div>
+        <div>
+          <span className="footer-title">公司</span>
+          <a className="link-hover link">关于</a>
+        </div>
+        <div>
+          <span className="footer-title">联系我们</span>
+          <a className="link-hover link">商业微信</a>
+          <a className="link-hover link">XiaoHong_yaokaixinya</a>
+          <Image src="/images/wangpan.png" alt="" width={60} height={60} />
+        </div>
+      </footer>
+      <div className="footer-center py-20">
+        <a className="link-hover link text-[#006CFFFF]">蜀引1CP备2021015500号</a>
+      </div>
+    </div>
+  )
+}
+
+export default FooterBar

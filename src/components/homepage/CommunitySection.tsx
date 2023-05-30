c24f241425bbc6503c575dde61ac77fdfabd41d7
HCX <1874640833@qq.com>
2023-05-30 16:13:09 +0800

    fix():qrcode

--- src/components/homepage/CommunitySection.tsx
+++ src/components/homepage/CommunitySection.tsx
@@ -42,7 +42,7 @@ const CommunitySection = ({
           </Link>
         </div>
         <div className="relative ml-3 h-12 w-40 md:h-16 md:w-48">
-          <div className="dropdown dropdown-hover">
+          <div className="dropdown-hover dropdown">
             <div className="btn_polygon_border h-12 w-40 bg-[#2c2c2c] md:h-16 md:w-48"></div>
             <Link
               href="https://join.slack.com/t/openimsdk/shared_invite/zt-1tmoj26uf-_FDy3dowVHBiGvLk9e5Xkg"
@@ -68,7 +68,7 @@ const CommunitySection = ({
               )}
             >
               <Image
-                src="https://s2.loli.net/2023/05/29/CZv8WirHy1LMaJD.jpg"
+                src="https://openim-1253691595.cos.ap-nanjing.myqcloud.com/WechatIMG20.jpeg"
                 alt=""
                 width={2187}
                 height={1669}

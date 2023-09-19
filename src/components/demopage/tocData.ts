835da0c18f76c429c2ed83f73b5f4e5bdfc39062
Bloomingg <1293499952@qq.com>
2023-09-19 19:53:42 +0800

    fix: pc v3.3.0 and pgyer host update

--- src/components/demopage/tocData.ts
+++ src/components/demopage/tocData.ts
@@ -5,9 +5,9 @@ export const toc_base_open: Demo[] = [
     label: "Flutter Android",
     icon: "/fonts/flutter_android.png",
     top_right_icon: "/fonts/qrcode.png",
-    qrcode: "/fonts/flutter_android_qrcode.png",
+    qrcode: "https://www.xcxwo.com/app/qrcode/OpenIM-Flutter",
     hover: false,
-    download: "https://www.pgyer.com/OpenIM-Flutter",
+    download: "https://www.xcxwo.com/OpenIM-Flutter",
     type: "mobile",
   },
   {
@@ -23,9 +23,9 @@ export const toc_base_open: Demo[] = [
     label: "Native Android",
     icon: "/fonts/native_android.png",
     top_right_icon: "/fonts/qrcode.png",
-    qrcode: "/fonts/native_android_qrcode.png",
+    qrcode: "https://www.xcxwo.com/app/qrcode/OpenIM-android",
     hover: false,
-    download: "https://www.pgyer.com/OpenIM-android",
+    download: "https://www.xcxwo.com/OpenIM-android",
     type: "mobile",
   },
   {
@@ -41,9 +41,9 @@ export const toc_base_open: Demo[] = [
     label: "uni-app Android",
     icon: "/fonts/native_android.png",
     top_right_icon: "/fonts/qrcode.png",
-    qrcode: "/fonts/uniapp_android_qrcode.png",
+    qrcode: "https://www.xcxwo.com/app/qrcode/OpenIM_Uniapp",
     hover: false,
-    download: "https://www.pgyer.com/OpenIM_Uniapp",
+    download: "https://www.xcxwo.com/OpenIM_Uniapp",
     type: "mobile",
   },
   {
@@ -59,7 +59,7 @@ export const toc_base_open: Demo[] = [
     top_right_icon: "/fonts/download.png",
     type: "pc",
     hover: false,
-    download: "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB-3.2.1.dmg",
+    download: "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB-3.3.0.dmg",
   },
   {
     label: "Electron for Mac M1",
@@ -68,7 +68,7 @@ export const toc_base_open: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB-3.2.1-arm64.dmg",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB-3.3.0-arm64.dmg",
   },
   {
     label: "Electron for Windows",
@@ -77,7 +77,7 @@ export const toc_base_open: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB%20Setup%203.2.1.exe",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB%20Setup%203.3.0.exe",
   },
   {
     label: "Electron for Ubuntu/UOS/Kylinos",
@@ -86,7 +86,7 @@ export const toc_base_open: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB_3.2.1_amd64.deb",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB_3.3.0_amd64.deb",
   },
   {
     label: "Electron for Ubuntu/UOS/Kylinos arm64",
@@ -95,7 +95,7 @@ export const toc_base_open: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB_3.2.1_arm64.deb",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CB_3.3.0_arm64.deb",
   },
 ];
 export const toc_enterprise: Demo[] = [
@@ -103,9 +103,9 @@ export const toc_enterprise: Demo[] = [
     label: "Flutter Android",
     icon: "/fonts/flutter_android.png",
     top_right_icon: "/fonts/qrcode.png",
-    qrcode: "/fonts/flutter_android_qrcode_c_e.png",
+    qrcode: "https://www.xcxwo.com/app/qrcode/OpenIM-Flutter-Full",
     hover: false,
-    download: "https://www.pgyer.com/OpenIM-Flutter-Full",
+    download: "https://www.xcxwo.com/OpenIM-Flutter-Full",
     type: "mobile",
   },
   {
@@ -121,9 +121,9 @@ export const toc_enterprise: Demo[] = [
     label: "Native Android",
     icon: "/fonts/native_android.png",
     top_right_icon: "/fonts/qrcode.png",
-    qrcode: "/fonts/native_android_qrcode_c_e.png",
+    qrcode: "https://www.xcxwo.com/app/qrcode/OpenIM-Enterprise",
     hover: false,
-    download: "https://www.pgyer.com/OpenIM-Enterprise",
+    download: "https://www.xcxwo.com/OpenIM-Enterprise",
     type: "mobile",
   },
   {
@@ -135,15 +135,15 @@ export const toc_enterprise: Demo[] = [
     download: "https://testflight.apple.com/join/cGuG9pVg",
     type: "mobile",
   },
-  // {
-  //   label: "uni-app Android",
-  //   icon: "/fonts/native_android.png",
-  //   top_right_icon: "/fonts/qrcode.png",
-  //   qrcode: "/fonts/uniapp_android_qrcode.png",
-  //   hover: false,
-  //   download: "https://www.pgyer.com/OpenIM_Uniapp",
-  //   type: "mobile",
-  // },
+  {
+    label: "uni-app Android",
+    icon: "/fonts/native_android.png",
+    top_right_icon: "/fonts/qrcode.png",
+    qrcode: "https://www.xcxwo.com/app/qrcode/OpenIM_Uniapp",
+    hover: false,
+    download: "https://www.xcxwo.com/OpenIM_Uniapp",
+    type: "mobile",
+  },
   {
     label: "Pc Web",
     icon: "/fonts/pc_web_toc.png",
@@ -157,7 +157,7 @@ export const toc_enterprise: Demo[] = [
     top_right_icon: "/fonts/download.png",
     type: "pc",
     hover: false,
-    download: "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER-3.2.1.dmg",
+    download: "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER-3.3.0.dmg",
   },
   {
     label: "Electron for Mac M1",
@@ -166,7 +166,7 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER-3.2.1-arm64.dmg",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER-3.3.0-arm64.dmg",
   },
   {
     label: "Electron for Windows",
@@ -175,7 +175,7 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER%20Setup%203.2.1.exe",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER%20Setup%203.3.0.exe",
   },
   {
     label: "Electron for Ubuntu/UOS/Kylinos",
@@ -184,7 +184,7 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.2.1_amd64.deb",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.3.0_amd64.deb",
   },
   {
     label: "Electron for Ubuntu/UOS/Kylinos arm64",
@@ -193,6 +193,6 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.2.1_arm64.deb",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.3.0_arm64.deb",
   },
 ];

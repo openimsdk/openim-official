03e977da0a3d54a493bb7d593eb83b421b2f9530
Bloomingg <1293499952@qq.com>
2023-10-23 17:24:19 +0800

    fix: words

--- src/dictionaries/zh.ts
+++ src/dictionaries/zh.ts
@@ -2,7 +2,7 @@ const zh = {
   home: {
     part1: {
       en: "Open source SDK for Instant Messaging",
-      text: "开源、免费、通用的即时通讯组件",
+      text: "开源、通用的即时通讯组件",
       desc: "OpenIM包括服务端和客户端SDK，实现了高性能、轻量级、易扩展等重要特性。开发者通过集成OpenIM SDK，并私有化部署服务端，可以将即时通讯、实时网络能力快速集成到自身应用中，确保业务数据的安全性和私密性。",
       btn_begin: "开始体验",
       btn_docs: "阅读文档",
@@ -10,7 +10,7 @@ const zh = {
     part2: {
       title_pre: "加入全球领先的",
       title: "IM开源社区",
-      desc: "OpenIM的开发者社区，覆盖个人开发者、初创企业和大型企业。",
+      desc: "OpenIM社区，覆盖个人开发者、初创企业和大型企业。",
       github: "查看GitHub",
       join: "加入微信社区",
       github_stars: "GITHUB STARS",
@@ -33,21 +33,21 @@ const zh = {
       title_pre: "单聊群聊满足各种",
       title: "社交场景",
       img: "/images/social_zh.png",
-      desc: "App管理员、群主、群管理员和普通成员四种角色。禁言、群公告、进群验证、群成员无上限、群消息按需加载等强大的群组功能。",
+      desc: "APP管理员、群主、群管理员和普通成员四种角色。禁言、群公告、进群验证、群成员无上限、群消息按需加载等强大的群组功能。",
     },
     part6: {
       title_pre: "特色",
       title: "功能",
       img: "/images/feature_zh.png",
-      desc: "支持自定义时长的阅后即焚私密聊天，消息编辑功能拓宽社交场景，让即时通讯多元化而更有趣。",
+      desc: "支持自定义时长的阅后即焚私密聊天，消息编辑功能拓宽社交场景，让即时通讯多元化且更有趣。",
     },
     part7: {
       title_pre: "OpenIM的",
       title: "优势",
-      sub_title: "开源、免费、通用的即时通讯组件。一键部署，轻松集成。",
+      sub_title: "开源、通用的即时通讯组件。一键部署，轻松集成。",
       t1: {
         title: "开源",
-        desc: "核心代码开源，数据自我掌控，打造全球领先的IM开源社区，包括客户端SDK和服务端，打造全球领先的IM开源社区。",
+        desc: "核心代码开源，数据自我掌控，打造全球领先的IM开源社区。",
       },
       t2: {
         title: "易扩展",
@@ -65,13 +65,13 @@ const zh = {
     part8: {
       title_pre: "建立在",
       title: "开源之上",
-      desc1: "OpenIM的核心--服务端以及客户端SDK--是永远免费和开源的。",
+      desc1: "OpenIM的核心--服务端以及客户端SDK--是永远开源的。",
       desc2: "不同平台的demo可供参考：",
     },
     part9: {
-      title_pre: "开源、免费、通用",
+      title_pre: "通用",
       title: "的即时通讯组件",
-      desc: "Open source, free, and universal instant messaging components",
+      desc: "开源、安全、可替代云服务商",
       btn_begin: "开始体验",
       btn_docs: "阅读文档",
     },

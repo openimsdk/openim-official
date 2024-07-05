25b3043e1c83fa1bef77b7bbe7e76be0e0662dc8
Bloomingg <1293499952@qq.com>
2024-07-05 21:00:16 +0800

    fix: update electron url

--- src/components/demopage/tocData.ts
+++ src/components/demopage/tocData.ts
@@ -166,7 +166,7 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.5.1_x64.dmg",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.8.0_x64.dmg",
   },
   {
     label: "Electron for Mac M1",
@@ -175,7 +175,7 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.5.1_arm64.dmg",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.8.0_arm64.dmg",
   },
   {
     label: "Electron for Windows",
@@ -183,7 +183,7 @@ export const toc_enterprise: Demo[] = [
     top_right_icon: "/fonts/download.png",
     type: "pc",
     hover: false,
-    download: "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.5.1.exe",
+    download: "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.8.0.exe",
   },
   {
     label: "Electron for Ubuntu/UOS/Kylinos",
@@ -192,7 +192,7 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.5.1_amd64.deb",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.8.0_amd64.deb",
     only_cn: true,
   },
   {
@@ -202,7 +202,7 @@ export const toc_enterprise: Demo[] = [
     type: "pc",
     hover: false,
     download:
-      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.5.1_arm64.deb",
+      "https://app-1302656840.cos.ap-nanjing.myqcloud.com/OpenIM-CER_3.8.0_arm64.deb",
     only_cn: true,
   },
 ];
